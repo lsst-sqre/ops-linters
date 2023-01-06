63d76ec5e07bc760afc24f5cd60f9c0f36bb3602
Christine Banek <cbanek@lsst.org>
2023-01-06 13:20:29 -0700

    [DM-26505] Add support for known subnets

--- route53-checker/checker.py
+++ route53-checker/checker.py
@@ -5,29 +5,42 @@ import dns
 
 logging.basicConfig(level=logging.INFO)
 
+known_subnets = [
+    '140.252.', # NOAO Tucson IP Subnet
+    '139.229.' # NOAO Tucson IP Subnet
+]
 
 # Get the DNS information
 zones = dns.list_all_hosted_zones()
 hosts = dns.query_dns_records(zones)
 
-for h in hosts:
-    logging.info(f"{h}: {hosts[h]}")
-
 # Get addresses information
 all_addresses = addresses.lookup_all_addresses()
-logging.info(all_addresses)
 
+dangling = {}
+not_dangling = {}
+
+for h in hosts:
+    for address in hosts[h]:
+        found = False
+        for subnet in known_subnets:
+            if address.startswith(subnet):
+                not_dangling[h] = address
+                found = True
+
+        if address in all_addresses:
+            not_dangling[h] = address
+            found = True
+
+        if not found:
+            dangling[h] = address
 
 logging.info("The following hostnames may be dangling:")
 
-for h in hosts:
-    for address in hosts[h]:
-        if address not in all_addresses:
-            logging.error(f"{h}: {address}")
+for h, a in dangling.items():
+    logging.error(f"{h}: {a}")
 
 logging.info("The following hostnames look good:")
 
-for h in hosts:
-    for address in hosts[h]:
-        if address in all_addresses:
-            logging.info(f"{h}: {address}")
+for h, a in not_dangling.items():
+    logging.info(f"{h}: {a}")
